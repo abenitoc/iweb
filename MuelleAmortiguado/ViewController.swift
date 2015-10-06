48b5d46377c2f58df2bc4cc35e9413bfd8e442e0
g336 DIT UPM <g336@l136.lab.dit.upm.es>
2015-10-06 15:40:47 +0200

    Initial Commit

---
+++ MuelleAmortiguado/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MuelleAmortiguado
+//
+//  Created by g336 DIT UPM on 6/10/15.
+//  Copyright © 2015 g336 DIT UPM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
