bff69e49dcbe7d29c0e30681f93589dbfbd3c8a8
g336 DIT UPM <g336@l136.lab.dit.upm.es>
2015-10-06 18:39:53 +0200

    spring

---
+++ MuelleAmortiguado/springModel.swift
@@ -0,0 +1,10 @@
+//
+//  springModel.swift
+//  MuelleAmortiguado
+//
+//  Created by g336 DIT UPM on 6/10/15.
+//  Copyright © 2015 g336 DIT UPM. All rights reserved.
+//
+
+import Foundation
+
---
+++ MuelleAmortiguado/springcontroller.swift
@@ -0,0 +1,9 @@
+//
+//  springcontroller.swift
+//  MuelleAmortiguado
+//
+//  Created by g336 DIT UPM on 6/10/15.
+//  Copyright © 2015 g336 DIT UPM. All rights reserved.
+//
+
+import Foundation
